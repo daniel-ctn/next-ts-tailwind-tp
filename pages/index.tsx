3587fcd2c299304bb30c8d482cc7eed670a144a4
Daniel <80741886+thangnguyen2359@users.noreply.github.com>
2021-12-18 00:38:14 +0700

    update config

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
-import type { NextPage } from "next";
-import Head from "next/head";
+import type { NextPage } from 'next'
+import Head from 'next/head'
 
 const Home: NextPage = () => {
   return (
@@ -7,20 +7,20 @@ const Home: NextPage = () => {
       <Head>
         <title>Next JS App with Tailwind</title>
         <meta
-          name="description"
-          content="Next JS project with tailwind template"
+          name='description'
+          content='Next JS project with tailwind template'
         />
       </Head>
 
       <body>
-        <main>
-          <div className="flex">
-            <h1 className="text-4xl font-bold">Daniel</h1>
+        <main className='h-screen'>
+          <div className='flex bg-gray-700 w-full h-full'>
+            <h1 className='text-4xl font-bold text-blue-200'>Daniel</h1>
           </div>
         </main>
       </body>
     </div>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
