ea3428dcfb46fce8323aba6cb76578e2711cd4a0
Daniel <80741886+thangnguyen2359@users.noreply.github.com>
2021-12-18 01:02:23 +0700

    add husky and lint staged setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,8 +14,11 @@ const Home: NextPage = () => {
 
       <body>
         <main className='h-screen'>
-          <div className='flex bg-gray-700 w-full h-full'>
+          <div className='flex bg-gray-700 w-full h-52 justify-center items-center space-x-6'>
             <h1 className='text-4xl font-bold text-blue-200'>Daniel</h1>
+            <button className='px-4 py-2 text-2xl text-cyan-200 font-bold bg-amber-800 hover:ring-4 rounded-2xl border-2'>
+              Nice!
+            </button>
           </div>
         </main>
       </body>
