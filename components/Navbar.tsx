7ef0d4f323202633d8a759dd92a08ec4f19548a0
Daniel <80741886+thangnguyen2359@users.noreply.github.com>
2021-12-18 22:41:36 +0700

    add config for absolute import

---
+++ components/Navbar.tsx
@@ -0,0 +1,9 @@
+import { FC, ReactElement } from 'react'
+
+interface NavbarProps {}
+
+const Navbar: FC = ({}: NavbarProps): ReactElement => {
+  return <div>navigation bar</div>
+}
+
+export default Navbar
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import { useEffect, useState } from 'react'
+import Navbar from 'components/Navbar'
 
 const Home: NextPage = () => {
   return (
@@ -15,6 +15,7 @@ const Home: NextPage = () => {
 
       <body>
         <main className='h-screen'>
+          <Navbar />
           <div className='flex bg-gray-700 w-full h-52 justify-center items-center space-x-6'>
             <h1 className='text-4xl font-bold text-blue-200'>Daniel</h1>
             <button className='px-4 py-2 text-2xl text-cyan-200 font-bold bg-amber-800 hover:ring-4 rounded-2xl border-2'>
