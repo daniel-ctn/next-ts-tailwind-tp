8eb66707966330c13db8ad33e4236a3be152bf0e
Daniel Nguyen <54087410+danielnguyen5201@users.noreply.github.com>
2022-04-20 13:42:09 +0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import Navbar from 'components/layout/Navbar'
 
 const Home: NextPage = () => {
   return (
-    <div>
+    <>
       <Head>
         <title>Next JS App with Tailwind</title>
         <meta
@@ -13,18 +13,16 @@ const Home: NextPage = () => {
         />
       </Head>
 
-      <body>
-        <main className='h-screen'>
-          <Navbar />
-          <div className='flex bg-gray-700 w-full h-52 justify-center items-center space-x-6'>
-            <h1 className='text-4xl font-bold text-blue-200'>Daniel</h1>
-            <button className='px-4 py-2 text-2xl text-cyan-200 font-bold bg-amber-800 hover:ring-4 rounded-2xl border-2'>
-              Nice!
-            </button>
-          </div>
-        </main>
-      </body>
-    </div>
+       <main className='h-screen'>
+         <Navbar />
+         <div className='flex bg-gray-700 w-full h-52 justify-center items-center space-x-6'>
+           <h1 className='text-4xl font-bold text-blue-200'>Daniel</h1>
+           <button className='px-4 py-2 text-2xl text-cyan-200 font-bold bg-amber-800 hover:ring-4 rounded-2xl border-2'>
+             Nice!
+           </button>
+         </div>
+       </main>
+    </>
   )
 }
 
