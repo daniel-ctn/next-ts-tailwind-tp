311ae261be4585eb82b840ec1cdf04cd8b94208f
Nguyen Cao Thang <thangnc2@dgroup.co>
2022-04-20 11:38:18 +0700

    update to React 18

--- components/Navbar.tsx
+++
@@ -1,9 +0,0 @@
-import { FC, ReactElement } from 'react'
-
-interface NavbarProps {}
-
-const Navbar: FC = ({}: NavbarProps): ReactElement => {
-  return <div>navigation bar</div>
-}
-
-export default Navbar
